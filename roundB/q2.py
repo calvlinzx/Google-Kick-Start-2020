f87d8687eef26f824e29e5dd9a9c2d8ee7538c72
calvlinzx <linzexin1214@gmail.com>
2020-04-20 01:14:56 +1000

    passed all tests for q2

--- roundB/q2.py
+++ roundB/q2.py
@@ -1,23 +1,12 @@
-from copy import deepcopy
 T = int(input())
 for x in range(1, T + 1):
     N,D = map(int, input().split())
     buses = list(map(int,input().split()))
-    first = buses[0]
-    i = D // first
-    y = 0
-    while i > 0:
-        alt = deepcopy(buses)
-        start = i * first
-        for j in range(start, D+1):
-            while alt:
-                if j % alt[0] == 0:
-                    alt.pop(0)
-                else:
-                    break
-        if not alt:
-            y = start
-            break
+    i = N -1
+    while D > 0 and i >= 0:
+        rest = D % buses[i]
+        if rest != 0:
+            D -= rest
         i -= 1
 
-    print("Case #{}: {}".format(x, y), flush = True)
+    print("Case #{}: {}".format(x, D), flush = True)
